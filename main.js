cad8937e13c3eec919f5fe697031b9a799dc19b3
sanjuraj9031 <sanjuraj.it2020@nsec.ac.in>
2024-02-24 13:04:15 +0530

    push

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Coding Enthusiast", "Web Developer"],
+    typedSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
